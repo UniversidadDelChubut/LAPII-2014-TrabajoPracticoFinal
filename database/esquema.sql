1fcc0603fbd94f7d9ccf5e95e542d64d49b6c29d
Alan <alan@Alan-pc.(none)>
2014-05-28 19:32:11 -0300

    cambios alan-fer

--- database/esquema.sql
+++ database/esquema.sql
@@ -13,8 +13,8 @@ CREATE TABLE materia (
 	anio int,
 	cuatrimestre int,
 
-	CONSTRAINT pk_materia PRIMARY KEY (codigo),
-	CONSTRAINT fk_materia FOREIGN KEY (carrera)
+	CONSTRAINT pk_materia PRIMARY KEY (carrera,codigo),
+	CONSTRAINT fk_carrera FOREIGN KEY (carrera)
 	REFERENCES carrera (codigo)
 );
 CREATE TABLE cursada (
@@ -22,38 +22,72 @@ CREATE TABLE cursada (
 	materia text NOT NULL,
 	anio int,
 	f_inicio date NOT NULL,
-	f_fin date NOT NULL
+	f_fin date NOT NULL,
 	cuatrimestre int,
 	porc_asistencia real NOT NULL,
 	
 	CONSTRAINT pk_cursada PRIMARY KEY (carrera, materia, anio),
-	CONSTRAINT fk_cursada FOREIGN KEY (carrera, codigo)
-	REFERENCES materia (codigo, carrera)
+	CONSTRAINT fk_cursada FOREIGN KEY (carrera, materia)
+	REFERENCES materia (carrera,codigo)
 	
 );
 CREATE TABLE comison (
-	id 
+	id serial,
 	carrera text  NOT NULL,
 	materia text NOT NULL,
-	anio int 
-	numero
+	anio int,
+	numero int,
 
 	CONSTRAINT pk_comision PRIMARY KEY (id),
-	CONSTRAINT fk_comison FOREIGN KEY (carrera, codigo, anio)
+	CONSTRAINT fk_cursada FOREIGN KEY (carrera, materia, anio)
 	REFERENCES cursada (carrera, materia, anio)
 );
 CREATE TABLE persona (
 	
 );
 CREATE TABLE profesor (
+	dni int PRIMARY KEY
 );
 CREATE TABLE alumno (
+	legajo int PRIMARY KEY
 );
 CREATE TABLE prof_comision (
+
 );
 CREATE TABLE clase (
+	id serial,
+	obligatorio BOOLEAN,
+	hora_inicio time NOT NULL,
+	hora_fin time NOT NULL,
+	aula text NOT NULL,
+	dictada BOOLEAN,
+	recuperatoria_de int,
+	comision int,
+	profesor int,
+	hora_ingreso_profesor time,
+	hora_salida_profesor time,
+	
+	CONSTRAINT pk_clase PRIMARY KEY(id),
+	CONSTRAINT fk_comison FOREIGN KEY(comision)
+	REFERENCES comison(id),
+	CONSTRAINT fk_recuperatoria_de FOREIGN KEY(recuperatoria_de)
+	REFERENCES clase(id)
 );
 CREATE TABLE asistencia (
+	comision int,
+	anio int,
+	clase int,
+	id serial,
+	presente BOOLEAN,
+	justificada BOOLEAN,
+
+	CONSTRAINT pk_asistencia PRIMARY KEY(id),
+	CONSTRAINT fk_comision FOREIGN KEY(comision)
+	REFERENCES comison(id),
+	CONSTRAINT fk_anio FOREIGN KEY(anio)
+	REFERENCES alumno(legajo),
+	CONSTRAINT fk_clase FOREIGN KEY(clase)
+	REFERENCES clase(id)
 );
 
 ROLLBACK;
