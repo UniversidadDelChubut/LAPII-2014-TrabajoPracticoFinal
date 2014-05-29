228cba72f790137aa43a61eb5cb4362f43035308
TerraToo <terratoo@gmail.com>
2014-05-28 22:00:57 -0300

    Envio codigo de Java de Comision

--- src/registroasistencias/Comision.java
+++ src/registroasistencias/Comision.java
@@ -1,13 +1,14 @@
 package registroasistencias;
 
-import java.util.List;
+import java.util.LinkedList;
 
 public class Comision {
 
 	private Cursada cursada;
 	private int numero;
-	private List <Alumno> alumnos= null;
-	private List <Clase> clases=null;
+	private List <Alumno> alumnos = new LinkedList<Alumno>(); 
+	private List <Clase> clases = new LinkedList<Clase>();
+
 	
 	public Comision(Cursada cursada, int numero){
 		this.cursada=cursada;
@@ -30,5 +31,45 @@ public class Comision {
 		return clases;
 	}
 	
+
+	public void agregarAlumno(Alumno alumno){ 
+	   
+	    if (alumnos.size() <= 30){
+	    	alumnos.add(alumno);
+	    }
+	    
+	    else{
+		 System.out.print("Comision llena !!!");
+	    }
+		
+	}*/
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cursada == null) ? 0 : cursada.hashCode());
+		result = prime * result + numero;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Comision other = (Comision) obj;
+		if (cursada == null) {
+			if (other.cursada != null)
+				return false;
+		} else if (!cursada.equals(other.cursada))
+			return false;
+		if (numero != other.numero)
+			return false;
+		return true;
+	}
 	
 }
